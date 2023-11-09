d4f9168c111cc076f9ee286aa6746e5e6758b420
Jenkinccc <50920648+Jenkinccc@users.noreply.github.com>
2023-11-09 17:50:46 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# daisyui-vue-vite-wep4sp
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/Jenkinccc/daisyui-vue-vite-wep4sp)
---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <button class="btn">Hello daisyui</button>
+  <button class="btn btn-secondary">Secondary</button>
+  <button class="btn btn-accent">Accent</button>
+  <button class="btn btn-ghost">Ghost</button>
+</template>
