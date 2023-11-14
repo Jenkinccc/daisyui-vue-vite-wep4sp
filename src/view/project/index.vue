ccb148044f380c64c14c7ba46603c3b187782f60
jenkinccc <jenkinccc@foxmail.com>
2023-11-14 18:02:22 +0800

    新增project

---
+++ src/view/project/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="bg-white py-24 sm:py-32">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <div 
+                class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+                <div class="bg-white dark:bg-slate-900 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
+                    <div class="group">
+                        <span class="inline-flex items-center justify-center p-2 bg-indigo-500 group-hover:rotate-45 rounded-md shadow-lg">
+                            <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><!-- ... --></svg>
+                        </span>
+                    </div>
+                    <h3 class="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">Writes Upside-Down
+                    </h3>
+                    <p class="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+                        The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works
+                        in outer
+                        space.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
